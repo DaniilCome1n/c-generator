8d1f8bf91cdcccb0ee20166c8ee1c7662cdcbbbb
Daniil <daniilkom1n@yandex.ru>
2021-05-25 23:04:45 +0300

    minor changes

---
+++ src/components/side-bar.vue
@@ -0,0 +1,41 @@
+<template>
+	<v-navigation-drawer app>
+		<div class="d-flex justify-center align-center">
+			<img :src="require('/src/assets/img/logo.png')" alt="logo">
+		</div>
+		<v-list nav active-class="deep-purple--text text--accent-4">
+			<v-list-item link>
+				<v-list-item-icon>
+					<v-icon>mdi-home</v-icon>
+				</v-list-item-icon>
+				<v-list-item-content>
+					<router-link to="/">Главная</router-link>
+				</v-list-item-content>
+			</v-list-item>
+			<v-list-item link>
+				<v-list-item-icon>
+					<v-icon>mdi-plus</v-icon>
+				</v-list-item-icon>
+				<v-list-item-content>
+					<router-link to="/create-layout">Создать креатив</router-link>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+
+<script>
+
+export default {
+	name: 'sidebar',
+	data() {
+		return {
+			// key: value
+		};
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/main-layout.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>main</div>
+</template>
+
+<script>
+export default {
+	name: 'main-layout',
+};
+</script>
+
+<style scoped>
+
+</style>
